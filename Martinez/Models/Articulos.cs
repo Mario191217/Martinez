868c3c6269e80344d373defeb71c5bf4e9c7a368
Mario Alvarenga <alvarengam666@gmail.com>
2020-09-06 10:50:56 -0600

    articulos

---
+++ Martinez/Models/Articulos.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Martinez.Models
+{
+    public class Articulos
+    {
+        [Key]
+        public int IdArticulo { get; set; }
+        public string Articulo { get; set; }
+        public double Cantidad { get; set; }
+        public double Precio { get; set; }
+        public string Descripcion { get; set; }
+        public DateTime UltimaCompra { get; set; }
+
+
+    }
+}
---
+++ Martinez/Models/Categorias.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Martinez.Models
+{
+    public class Categorias
+    {
+        [Key]
+        public int IdCategoria { get; set; }
+        public string Categoria { get; set; }
+        public string Descripcion { get; set; }
+    }
+}
